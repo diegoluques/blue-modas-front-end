86f698c57c37a4493098f7263dcd05e779870673
Diego Luques <luquesdiego@outlook.com>
2021-04-13 13:17:18 -0400

    Criado função para salvar o produto

--- src/app/views/adm/produto-cadastro/produto-cadastro.component.ts
+++ src/app/views/adm/produto-cadastro/produto-cadastro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProdutoService } from 'src/app/services/produto.service';
 
 @Component({
   selector: 'app-produto-cadastro',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutoCadastroComponent implements OnInit {
 
-  constructor() { }
+  nome: String;
+  preco: String;
+  imagem: String;
+  loading: boolean;
+
+  constructor(
+    private router: Router,
+    private produtoService: ProdutoService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  cadastrarProduto() {
+    this.loading = true;
+
+    let data = {
+      nomeProduto: this.nome,
+      precoProduto: this.preco,
+      urlImage: this.imagem
+    };
+    
+    this.produtoService.cadastrarProduto(data).subscribe(data => {
+      this.loading = false;
+      this.router.navigate(['adm/produto-listagem']);
+    });
   }
 
 }
