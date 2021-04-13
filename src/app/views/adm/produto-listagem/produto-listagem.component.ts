194c261897cf4d1c780ab452dfda28dc4ad6bdd8
Diego Luques <luquesdiego@outlook.com>
2021-04-13 12:22:32 -0400

    Criado a tela de listagem de produtos

---
+++ src/app/views/adm/produto-listagem/produto-listagem.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Produto } from 'src/app/models/produto';
+import { ProdutoService } from 'src/app/services/produto.service';
+
+@Component({
+  selector: 'app-produto-listagem',
+  templateUrl: './produto-listagem.component.html',
+  styleUrls: ['./produto-listagem.component.css']
+})
+export class ProdutoListagemComponent implements OnInit {
+
+  produtos: Produto[];
+  totalItensCarrinho: number;
+
+  constructor(
+    private produtoService: ProdutoService,
+  ) { }
+
+  ngOnInit(): void {
+    this.obterProdutros();
+  }
+
+  obterProdutros() {
+    this.produtoService.obterProdutros().subscribe(data => {
+      this.produtos = data
+    })
+  }
+
+  deletarItem(idProduto: number) {
+    this.produtoService.deletarProduto(idProduto).subscribe(data => {
+      this.obterProdutros();
+    });
+  }
+
+}
